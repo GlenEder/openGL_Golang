903d542fe3f0b12145f6c437c84abed0c162705d
Glen Eder <glen.ma.eder@gmail.com>
2021-03-01 10:41:53 -0500

    Initalizes glfw

---
+++ go.mod
@@ -0,0 +1,5 @@
+module openGL_Golang
+
+go 1.16
+
+require github.com/go-gl/glfw/v3.3/glfw v0.0.0-20201108214237-06ea97f0c265
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "runtime"
+import (
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"runtime"
+)
 
 func init() {
 	//lock onto main thread for glfw and gl calls
 	runtime.LockOSThread()
+
+	//Initialize glfw
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
