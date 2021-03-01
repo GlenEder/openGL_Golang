e0fdf2a08fb5dcd740032fa102be5f489740d1e8
Glen Eder <glen.ma.eder@gmail.com>
2021-03-01 10:56:36 -0500

    Adds create window method

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"runtime"
 )
 
+const (
+	//window width and height
+	WIDTH = 480
+	HEIGHT = 480
+)
+
 func init() {
 	//lock onto main thread for glfw and gl calls
 	runtime.LockOSThread()
@@ -17,6 +23,38 @@ func init() {
 
 func main() {
 
+	window := createMainWindow()
+	defer window.Destroy()
+	defer glfw.Terminate()
 
+	//render loop
+	for !window.ShouldClose() {
 
+	}
+
+}
+
+func createMainWindow() *glfw.Window {
+
+	//Describe window we're about to make
+	// 4x antialiasing
+	glfw.WindowHint(glfw.Samples, 4)
+	//Request openGl v4.1
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	//For mac compatibility
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	//create window
+	window, err := glfw.CreateWindow(WIDTH, HEIGHT, "OpenGL-Tutorial.org", nil, nil)
+	if err != nil {
+		glfw.Terminate()
+		panic(err)
+	}
+
+	//bring focus to window
+	window.MakeContextCurrent()
+
+	return window
 }
